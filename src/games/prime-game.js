0a0e0dd8f0fd839607d51519ffb26e429dcc1cc6
gobacktosleep <egredimini@gmail.com>
2022-11-11 19:58:34 +0300

    add brain-prime game

---
+++ src/games/prime-game.js
@@ -0,0 +1,26 @@
+import startGame from '../index.js';
+import getRandomNumber from '../randomizerModule.js';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+};
+
+const startRound = () => {
+  const randomNumber = getRandomNumber();
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
+
+  return [randomNumber, correctAnswer];
+};
+
+const primeGame = () => {
+  startGame(gameDescription, startRound);
+};
+
+export default primeGame;
